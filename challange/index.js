eed1fc4029ede36b7356d177792b453a3ca5abe9
batataazul <gabrieldefreitasgarcia@outlook.com>
2022-02-26 14:57:41 -0300

    created csv parser class

--- challange/index.js
+++ challange/index.js
@@ -1,8 +1,27 @@
-import { parse } from "csv-parse";
-import { createReadStream } from "fs";
+import { parse } from "csv-parse/sync";
+import fs from "fs";
+import pkg  from "lodash";
+const { ld } = pkg;
+import pkg2 from "google-libphonenumber";
+const { glp } = pkg2;
 
-let parser = parse({ cast: true }, function (err, records) {
-    console.log(records);
-});
+class CsvParser{
+    constructor(path) {
+        this.getData = this.getData.bind(this);
+        let data;
+        (async function () {
+            const text = fs.readFileSync(path,"utf-8");
+            data = parse(text, { cast: true });
+        })();
+        this._data = data;
+    }
 
-createReadStream("input.csv").pipe(parser);
+    getData() {
+        return this._data;
+    }
+}
+
+(function (){
+    let parser = new CsvParser("input.csv");
+    console.log(parser.getData()[0]);
+})()
